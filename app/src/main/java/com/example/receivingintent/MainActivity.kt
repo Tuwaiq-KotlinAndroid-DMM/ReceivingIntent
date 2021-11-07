e0f54033c054661b15ff8d58699499b9a08500e2
saadiqbalkhan29 <saad.iqbal@tuwaiq.edu.sa>
2021-11-07 20:20:20 +0300

    Receiving Intent Completed with comments

--- app/src/main/java/com/example/receivingintent/MainActivity.kt
+++ app/src/main/java/com/example/receivingintent/MainActivity.kt
@@ -16,33 +16,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Get the context of ImageView and TextView
         val picView: ImageView = findViewById(R.id.imageView)
         val txtView: TextView = findViewById(R.id.textView)
 
         // Receiving the intent
         val receivedIntent = intent
+
+        // Parsing the Intent action information from the received intent
         val receivedAction = receivedIntent.action
         Log.d(TAG, receivedAction.toString())
+
+        // Parsing the Intent type information from the received intent
         val receivedType = receivedIntent.type
         Log.d(TAG, receivedType.toString())
 
+        // Checking, if the received type is not null and starts with "text/, so execute the if block
         if(receivedType != null && receivedType!!.startsWith("text/")){
+
+            // Hide the ImageView
             picView.setVisibility(View.GONE)
 
             // Gets the received Text
             val receivedText = receivedIntent.getStringExtra(Intent.EXTRA_TEXT)
+
+            // Handle the null condition of receivedText
             if(receivedText != null){
+                // Display the received text
                 txtView.setText(receivedText)
             }
 
         }
+        // Checking, if the received type is not null and starts with "image/, so execute the if block
         else if(receivedType != null && receivedType!!.startsWith("image/")){
+
+            // Hide the textView
             txtView.setVisibility(View.GONE)
 
             // Gets the received URI of the image
             val receivedURI: Uri? = receivedIntent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as Uri?
 
+            // Handle the null condition of receivedURI
             if(receivedURI != null){
+                // Display the received image
                 picView.setImageURI(receivedURI)
             }
         }
@@ -50,9 +66,3 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-//Implicit Intents - URL Lab
-// 1. Create a new Android Project.
-// 2. Name it: ImplicitIntentsURLLab
-// 3. In activity_main.xml, create a EditText field
-// 4. We will use the text that user enters and upon button press we will use implicit intent to instruct android to open link we provided.
-// 5. Now any URL we enter in text field will be opened in available browser.
